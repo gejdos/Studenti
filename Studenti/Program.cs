a05a11ad46c94f81e9e06bef8b6f905e97ebf295
Jakub Gejdos <gejdosjakub@gmail.com>
2017-12-04 11:44:16 +0100

    commit

--- Studenti/Program.cs
+++ Studenti/Program.cs
@@ -6,13 +6,10 @@ namespace Studenti
     {
         static void Main(string[] args)
         {
-            while (true)
-            {
-                StudentsDB databaza = new StudentsDB();
-                databaza.VytvorDatabazu();
+            StudentsDB databaza = new StudentsDB();
+            databaza.VytvorDatabazu();
 
-                Console.ReadKey();
-            }
+            Console.ReadKey();
         }
     }
 }
