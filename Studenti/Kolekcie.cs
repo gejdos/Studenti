310904f7d2096e214cd905cc951fa80aafcd8d80
Jakub Gejdos <gejdosjakub@gmail.com>
2017-12-08 20:24:06 +0100

    kolekcie

---
+++ Studenti/Kolekcie.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Studenti
+{
+    class Kolekcie
+    {
+        //fronta FIFO - "first in, first out"
+        Queue fronta = new Queue();
+
+
+    }
+}
